3358da3bf299586a20c1a9cf9591093226aec84a
Kir-Antipov <kp_antipov@mail.ru>
2019-07-30 03:40:37 +0300

    Assembly has been signed

--- KE.VSIX/Properties/AssemblyInfo.cs
+++ KE.VSIX/Properties/AssemblyInfo.cs
@@ -1,19 +1,18 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("KE.VSIX")]
-[assembly: AssemblyDescription("KE.VSIX - a set of useful tools for developing extensions for Visual Studio. \r\n\r\nDon't use this project without prior modification!!!")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Kir_Antipov")]
-[assembly: AssemblyProduct("KE.VSIX")]
-[assembly: AssemblyCopyright("Kir_Antipov ©  2019")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTitle("KE.VSIX")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("KE.VSIX")]
+[assembly: AssemblyCompany("Kir_Antipov")]
+[assembly: AssemblyCopyright("Kir_Antipov © 2019")]
+[assembly: AssemblyDescription("KE.VSIX - a set of useful tools for developing extensions for Visual Studio. \r\n\r\nDon't use this project without prior modification!!!")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("e7e4b189-6cd1-4a2d-bdd8-27a667885094")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
